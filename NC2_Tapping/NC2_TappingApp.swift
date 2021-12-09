5aa073d7d51b9f9b460488c31d1929e55ad9f122
Nicolas Mariniello <seldon1000@tutanota.com>
2021-12-09 12:31:29 +0100

    Initial Commit

---
+++ NC2_Tapping/NC2_TappingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC2_TappingApp.swift
+//  NC2_Tapping
+//
+//  Created by Nicolas Mariniello on 09/12/21.
+//
+
+import SwiftUI
+
+@main
+struct NC2_TappingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
