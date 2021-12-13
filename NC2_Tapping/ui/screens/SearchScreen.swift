741e8a35e8c13c8e2b5a9dac48ee8bd8061bbcd2
Nicolas Mariniello <seldon1000@tutanota.com>
2021-12-13 17:22:54 +0100

    Refactor

--- NC2_Tapping/ui/screens/SearchScreen.swift
+++ NC2_Tapping/ui/screens/SearchScreen.swift
@@ -12,37 +12,34 @@ struct SearchScreen: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Results")
+            Text(searchedText.isEmpty ? "Discover" : "Results")
                 .font(.system(size: 22, weight: .bold))
                 .padding(.horizontal)
-            List {
-                ForEach((courses.filter { it in it.title.lowercased().starts(with: searchedText.lowercased()) ||
-                    it.title.lowercased().contains(searchedText.lowercased()) ||
-                    it.interest.lowercased().starts(with: searchedText.lowercased()) ||
-                    it.interest.lowercased().contains(searchedText.lowercased()) ||
-                    it.searchCategory == searchedText
-                })) { course in
-                    NavigationLink(destination: CourseDetailsScreen(course: course)) {
-                        ListItem(course: course)
+            ZStack {
+                List {
+                    ForEach(0..<searchCategories.count) { i in
+                        SearchCategoryButton(searchedText: $searchedText, searchCategory: searchCategories[i])
                     }
-                    .buttonStyle(.plain)
                 }
-            }
-            .listStyle(.plain)
-        }
-        .hidden(searchedText.isEmpty)
-        VStack(alignment: .leading) {
-            Text("Discover")
-                .font(.system(size: 22, weight: .bold))
-                .padding(.horizontal)
-            List {
-                ForEach(0..<searchCategories.count) { i in
-                    SearchCategoryButton(searchedText: $searchedText, searchCategory: searchCategories[i])
+                .listStyle(.plain)
+                .hidden(!searchedText.isEmpty)
+                List {
+                    ForEach((courses.filter { it in it.title.lowercased().starts(with: searchedText.lowercased()) ||
+                        it.title.lowercased().contains(searchedText.lowercased()) ||
+                        it.interest.lowercased().starts(with: searchedText.lowercased()) ||
+                        it.interest.lowercased().contains(searchedText.lowercased()) ||
+                        it.searchCategory == searchedText
+                    })) { course in
+                        NavigationLink(destination: CourseDetailsScreen(course: course)) {
+                            ListItem(course: course)
+                        }
+                        .buttonStyle(.plain)
+                    }
                 }
+                .listStyle(.plain)
+                .hidden(searchedText.isEmpty)
             }
-            .listStyle(.plain)
         }
-        .hidden(!searchedText.isEmpty)
     }
 }
 
